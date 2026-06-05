2d0760b5bcc301f68a8ab7a63f924cc98d007d35
Alaskh4 <rf72055@gmail.com>
2026-06-05 19:31:43 -0300

    Fix spelling of 'Herói' and 'Poção de Vida'

--- Jogo.cs
+++ Jogo.cs
@@ -1,10 +1,12 @@
+using System;
+
 public class Jogo
 {
     public void Iniciar()
     {
-        Personagem jogador = new Personagem("Herói", 100, 20);
+        Personagem jogador = new Personagem("Heroi", 100, 20);
         Inimigo inimigo = new Inimigo("Goblin", 50);
-        Item pocao = new Item("Poção de Vida");
+        Item pocao = new Item("Pocao de Vida");
 
         jogador.Atacar();
         inimigo.ReceberDano(jogador.Ataque);
