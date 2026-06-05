68127f6ffb2c872e2f95180acc6a1017ed0b1117
Alaskh4 <rf72055@gmail.com>
2026-06-05 19:22:42 -0300

    Create Item.cs

---
+++ Item.cs
@@ -0,0 +1,14 @@
+public class Item
+{
+    public string Nome { get; set; }
+
+    public Item(string nome)
+    {
+        Nome = nome;
+    }
+
+    public void Usar()
+    {
+        Console.WriteLine("Você utilizou o item " + Nome + ".");
+    }
+}
